03ad4376013ac86c2ddca0789717174f6e560947
song <1044241610@qq.com>
2020-11-07 12:19:02 +0800

    update

--- sir/ode.py
+++ sir/ode.py
@@ -3,8 +3,9 @@ import numpy as np
 import sympy as sym
 import matplotlib.pyplot as plt
 
-class population:
-    def __init__(self, S, I, R):
+class covid():
+    def __init__(self, S, I ,R, b, k, **kwargs):
+        # todo 1. here I introduce **kwargs for further variations
         assert type(S) == int, 'S should be int'
         assert type(I) == int, 'I should be int'
         assert type(R) == int, 'R should be int'
@@ -18,33 +19,58 @@ class population:
         self.i = I / self.N
         self.r = R / self.N
 
-class covid(population):
-    def __init__(self, S, I ,R, k, b, **kwargs):
-        # todo 1. here I introduce **kwargs for further variations
-        super(covid, self).__init__(S, I, R)
-
         self.k = k
         self.b = b
+
+        self.sol = None
         # below, we should add more init for further variations.
 
-    def fun(self, t, y):
-        s_ = -self.b * y[0] * y[1]
-        i_ = self.b * y[0] * y[1] - self.k * y[1]
-        r_ = self.k * y[1]
-        return np.array([s_, i_, r_])
+    def __call__(self, t):
+        """
+        return proportion of each group at particular time
+        """
+        # raise error if the solve method has not ran
+        if self.sol is None:
+            raise AssertionError('covid class is callable after use solve method')
 
-    def solve(self, t_span, fun=None, **kwargs):
+        return self.sol.sol(t)
+
+    def __repr__(self):
+        return "Covid({}, {}, {})(b={}, k={})".format(self.s, self.i, self.r, self.b, self.k)
+
+
+    def solve(self, t_bound, fun=None, h=1, **kwargs):
+        """
+        define ode solve for this system
+        input t_bound to simulation for t days
+        default function is self.fun, default step size is h=1
+        """
         if fun is None:
-            fun = self.fun
+            def fun(t, y):
+                """
+                define initial function, the basic sir differential equations
+                """
+                s_ = -self.b * y[0] * y[1]
+                i_ = self.b * y[0] * y[1] - self.k * y[1]
+                r_ = self.k * y[1]
+                return np.array([s_, i_, r_])
+
         y0 = np.array([self.s, self.i, self.r])
-        self.sol = solve_ivp(fun, t_span, y0=y0, **kwargs)
+        t_span = (0, t_bound)
+        t_eval = range(1, t_bound+1, h)
+
+        self.sol = solve_ivp(fun, t_span, y0=y0, t_eval=t_eval, dense_output=True, **kwargs)
+
         return self.sol
 
-    def plot(self, *args, **kwargs):
-        plt.plot(self.sol.t, self.sol.y[0], label='Susceptible', col='green')
-        plt.plot(self.sol.t, self.sol.y[1], label='Infectious', col='red')
-        plt.plot(self.sol.t, self.sol.y[2], label='Removed', col='blue')
-        plt.title("SIR simulation")
+    def plot(self):
+        """
+        integrate plot related lines
+        """
+        plt.plot(self.sol.t, self.sol.y[0], label='Susceptible', color='green')
+        plt.plot(self.sol.t, self.sol.y[1], label='Infectious', color='red')
+        plt.plot(self.sol.t, self.sol.y[2], label='Removed', color='blue')
+        plt.title("SIR ODE simulation, b={},k={}".format(self.b, self.k))
         plt.ylabel("ratio")
         plt.xlabel("day")
         plt.legend()
--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import unittest
-
+from sir.ode import *
 
 class TestABM(unittest.TestCase):
     def setUp(self):
@@ -13,8 +13,21 @@ class TestODE(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_ode(self):
+        N = 1_000_000
+        I = 5
+        R = 0
+        S = N - I - R
+        k = 0.01
+        b = 1
+        ode_covid = covid(S, I, R, b, k)
+        ode_covid.solve(t_bound=100)
+        t = np.arange(5, 95, 5)
+        s, i, r = ode_covid(t)
+
+        lhs = (ode_covid(t + 0.1) - ode_covid(t)) / 0.1
+        rhs = np.array([-b * s * i, b * s * i - k * i, k * i])
+        self.assertTrue(np.all(np.abs(lhs - rhs) < 0.01))
 
 
 if __name__ == '__main__':
