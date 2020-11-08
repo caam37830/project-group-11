97d978d35187dc876bf02893cb44dbfbca6ccb37
song <1044241610@qq.com>
2020-11-08 10:24:05 +0800

    update ode scrit

---
+++ scripts/ode_scripts.py
@@ -0,0 +1,16 @@
+from sir.ode import covid
+
+# init population
+N = 1_000_000
+I = 5
+R = 0
+S = N - I - R
+
+# init the covid class
+ode_covid = covid(S, I, R, k=0.05, b=3)
+# solve that class
+ode_covid.solve(t_bound=365)
+# plot the numerical solution
+ode_covid.plot()
+# s, i, r of day 150
+print(ode_covid(150))
