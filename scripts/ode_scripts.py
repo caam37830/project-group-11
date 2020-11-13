b1f09bfc65c408a9744c9a3d8277f1d01e78053e
James Keane <yahmskeano@gmail.com>
2020-11-12 20:23:36 -0600

    Revert temporary testing changes

--- scripts/ode_scripts.py
+++ scripts/ode_scripts.py
@@ -3,32 +3,32 @@ sys.path.append("../")
 from sir.ode import covid, phase_plot
 
 # init population
-N = 100_000
-I = 100
+N = 1_000_000
+I = 5
 R = 0
 S = N - I - R
 
 # init the covid class
-ode_covid = covid(S, I, R, b=3, k=0.1)
+ode_covid = covid(S, I, R, b=3, k=0.01)
 # solve that class
-ode_covid.solve(t_bound=100)
+ode_covid.solve(t_bound=365)
 # plot the numerical solution
-ode_covid.plot(save_path=None)
+ode_covid.plot('../doc/checkpoint/figures/ode1.png')
 # s, i, r of day 150
 print(ode_covid(100))
 
 
 # init the covid class
 ode_covid = covid(S, I, R, b=3, k=0.1)
-ode_covid.solve(t_bound=100)
-ode_covid.plot(save_path=None)
+ode_covid.solve(t_bound=365)
+ode_covid.plot('../doc/checkpoint/figures/ode2.png')
 
 # init the covid class
-ode_covid = covid(S, I, R, b=0.8, k=0.1)
-ode_covid.solve(t_bound=100)
-ode_covid.plot(save_path=None)
+ode_covid = covid(S, I, R, b=0.8, k=0.01)
+ode_covid.solve(t_bound=365)
+ode_covid.plot('../doc/checkpoint/figures/ode3.png')
 
 # Phase diagram with b, k axes with portion of population that is infected at time t 
-phase_plot(N, I, R, t=5, phase='I', save_path=None)
-phase_plot(N, I, R, t=10, phase='I', save_path=None)
-phase_plot(N, I, R, t=50, phase='I', save_path=None)
+phase_plot(N, I, R, t=5, phase='I', save_path='../doc/checkpoint/figures/phase_diagram1.png')
+phase_plot(N, I, R, t=10, phase='I', save_path='../doc/checkpoint/figures/phase_diagram2.png')
+phase_plot(N, I, R, t=50, phase='I', save_path='../doc/checkpoint/figures/phase_diagram3.png')
