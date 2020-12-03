b9c561e8b7076bfbb49113fb3f15262eae994e08
James Keane <yahmskeano@gmail.com>
2020-12-02 18:17:06 -0600

    Updated functions in abm.py to take grid-based populations. Added 'ob' parameter for chance of non-neighbor interaction. Updated abm_sim script to reflect parameter changes

--- scripts/abm_sim.py
+++ scripts/abm_sim.py
@@ -5,18 +5,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Create 3 time plots at various b, k values
-pop = new_pop(10_000, 1)
-time_plot(pop, 3, .01, 365, '../doc/checkpoint/figures/abm1.png')
-pop = new_pop(10_000, 1)
-time_plot(pop, 3, .1, 365, '../doc/checkpoint/figures/abm2.png')
-pop = new_pop(10_000, 1)
-time_plot(pop, 1, .01, 365, '../doc/checkpoint/figures/abm3.png')
+pop = new_pop(1, 100, 100)
+time_plot(pop, 3, 0, .01, 365, '../doc/checkpoint/figures/abm1.png')
+pop = new_pop(1, 100, 100)
+time_plot(pop, 3, 0, .1, 365, '../doc/checkpoint/figures/abm2.png')
+pop = new_pop(1, 100, 100)
+time_plot(pop, 1, 0, .01, 365, '../doc/checkpoint/figures/abm3.png')
 
 # Code for phase plots
 
 bs=np.arange(1, 11, dtype=np.int64)
-ks=np.linspace(0.01, .5,10)
+ks=np.linspace(0.01, .5,25)
 
-abm_phase(10_000, 1, t=5, ks=ks, bs=bs, save_path='../doc/checkpoint/figures/abm_phase_diagram1.png')
-abm_phase(10_000, 1, t=10, ks=ks, bs=bs, save_path='../doc/checkpoint/figures/abm_phase_diagram2.png')
-abm_phase(10_000, 1, t=50, ks=ks, bs=bs, save_path='../doc/checkpoint/figures/abm_phase_diagram3.png')
+abm_phase(100, 100, 1, t=5, ks=ks, bs=bs, save_path='../doc/checkpoint/figures/abm_phase_diagram1.png')
+abm_phase(100, 100, 1, t=10, ks=ks, bs=bs, save_path='../doc/checkpoint/figures/abm_phase_diagram2.png')
+abm_phase(100, 100, 1, t=50, ks=ks, bs=bs, save_path='../doc/checkpoint/figures/abm_phase_diagram3.png')
+
--- sir/abm.py
+++ sir/abm.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from numba import njit
 
 class Person():
     """
@@ -47,7 +48,7 @@ class Person():
         if self.state == 'I':
             self.state = 'R'
 
-def abm_pop_sim(pop, b, k, t):
+def abm_pop_sim(pop, b, ob, k, t):
     """
     Simulate the spread of a disease on a given population over t days.
     pop designates the starting state, b and k control the spread and recovery rate
@@ -57,7 +58,7 @@ def abm_pop_sim(pop, b, k, t):
     R = []
 
     for i in range(t):
-        pop = infect_pop(pop, b)
+        pop = infect_pop(pop, b, ob)
         pop = remove_pop(pop, k)
         S.append(len(get_indices(pop, 'S')))
         I.append(len(get_indices(pop, 'I')))
@@ -65,49 +66,110 @@ def abm_pop_sim(pop, b, k, t):
 
     return pop, S, I, R
 
+
 def remove_pop(pop, k):
     """
     Remove k proportion of the infected population 
     """
+    nrow, ncol = pop.shape
+
     # Remove population
     infected = get_indices(pop, 'I')
-
+    
     # Determine the rounded number of infected to remove
     removals = np.int(np.floor(len(infected) * k))
 
     # Remove k proportion of infected
-    for i in random.sample(infected, removals):
-        pop[i].remove()
+    for rem in random.sample(infected, removals):
+        i = rem // nrow
+        j = rem % ncol
+        pop[i, j].remove()
     
     return pop
 
-def infect_pop(pop, b):
+
+def infect_pop(pop, b, ob):
     """
     Have each infected agent interact with b others
     """
     # Find infected agents
     infected = get_indices(pop, 'I')
-    pop_size = len(pop)
+
+    nrow, ncol = pop.shape
 
     # Infect population 
-    for i in infected:
+    for inf in infected:
 
-        # Identify the b interactions for this agent
-        receivers = random.sample(range(0,pop_size), b)
+        # Find grid index of infected
+        i = inf // nrow
+        j = inf % ncol
+        nbrs = neighbors(i, j, nrow, ncol)
 
-        for j in receivers:
-            pop[j].infect()
+        # Identify the b nbr interactions
+        # Only use b if there are b neighbors to interact with!
+        nei_interactions = min(b, len(nbrs))
+        receivers = random.sample(nbrs, nei_interactions)
+
+        # Infect neighbors
+        for rec in receivers:
+            pop[rec].infect()
+
+        # Check for interacting outside of cohort
+        if np.random.rand(1) < ob:
+            continue
+
+        # Infect a random other agent
+        out_i = np.random.choice(nrow)
+        out_j = np.random.choice(ncol)
+        pop[out_i,out_j].infect()
+        
 
     return pop
 
+def neighbors(i, j, m, n):
+    """
+    Returns the neighboring indices for a given index
+    in a matrix  of m x n shape
+    """
+
+    # Sets default delta indices
+    # Adjust delta indices for given index on edge of matrix
+    inbrs = [-1, 0, 1]
+    if i == 0:
+        inbrs = [0, 1]
+    if i == m-1:
+        inbrs = [-1, 0]
+    jnbrs = [-1, 0, 1]
+    if j == 0:
+        jnbrs = [0, 1]
+    if j == n-1:
+        jnbrs = [-1, 0]
+
+    nbrs = []
+    # Applies deltas and yields neighboring indices
+    for delta_i in inbrs:
+        for delta_j in jnbrs:
+            if delta_i == delta_j == 0:
+                continue
+            else:
+                nbrs.append((i+delta_i, j+delta_j))
+
+    return nbrs
+
+                
+
 def get_indices(pop, state):
     """
-    Finds the population agents with a given state
+    Finds the population agents with a given state.
+    Flattens the population for easier index finding.
     """
-    indices = [i for i, agent in enumerate(pop) if agent.cur_state() == state]
+    # Convert from grid pop to flattened pop
+    pop_flat = pop.flatten()
+
+    indices = [i for i, agent in enumerate(pop_flat) if agent.cur_state() == state]
     return indices
 
-def abm_phase(N, infected, t, bs=np.arange(1, 11, dtype=np.int64), ks=np.linspace(0.01, .5, 10), save_path=None):
+def abm_phase(nrow, ncol, infected, t, bs=np.arange(1, 11, dtype=np.int64), ks=np.linspace(0.01, .5, 10), save_path=None):
     """
     plot phase diagram
     :param N: starting population
@@ -120,12 +182,14 @@ def abm_phase(N, infected, t, bs=np.arange(1, 11, dtype=np.int64), ks=np.linspac
     """
     # store initial state of pop for future use
     cts = np.zeros((len(bs), len(ks)))
+    N = nrow * ncol
+    ob = 0
 
     for i, b in tqdm(enumerate(bs)):
         # ensure b is an int, not a float
         for j, k in enumerate(ks):
-            pop = new_pop(N, infected)
-            pop, S, I, R = abm_pop_sim(pop, b, k, t)
+            pop = new_pop(infected, nrow, ncol)
+            pop, S, I, R = abm_pop_sim(pop, b, ob, k, t)
             cts[i, j] = np.true_divide(I[-1], N)
 
 
@@ -142,28 +206,34 @@ def abm_phase(N, infected, t, bs=np.arange(1, 11, dtype=np.int64), ks=np.linspac
         fig.savefig(save_path)
     plt.show()
 
-def new_pop(N, infected):
+def new_pop(start_inf, nrow, ncol):
     """
     Create an initial state population
     :param N: population size
-    :param infected: count of infected initial state
+    :param start_inf: count of infected initial state
     """
     # Create a population of susceptibles
-    pop = [Person() for i in range(N)]
+    pop = [Person() for i in range(nrow * ncol)]
+    pop_size = len(pop)
 
+    infected = random.sample(range(0,pop_size), start_inf)
     # Infect a given number of the population
-    for i in range(infected):
+    for i in infected:
         pop[i].infect()
-    return pop
+    
+    # Move population into a grid
+    pop_grid = np.reshape(pop, (nrow, ncol))
+   
+    return pop_grid
 
 
-def time_plot(pop, b, k, t, save_path):
+def time_plot(pop, b, ob, k, t, save_path):
     """
     Runs a simulation with the given parameters and 
     Outputs a plot of the three state ratios over time t
     """
-    N = len(pop)
-    pop, S, I, R = abm_pop_sim(pop, b, k, t)
+    N = pop.size
+    pop, S, I, R = abm_pop_sim(pop, b, ob, k, t)
 
     s = np.true_divide(S, N)
     i = np.true_divide(I, N)
