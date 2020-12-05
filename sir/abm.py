2fea6c7cdb6f2354c2adda4fe3aee0345ed05af7
James Keane <yahmskeano@gmail.com>
2020-12-04 18:41:51 -0600

    All of abm.py should be set. Plots ready to run in the script file, just depends if I want to add strict cohorts or not.

--- sir/abm.py
+++ sir/abm.py
@@ -110,6 +110,14 @@ def infect_pop(pop, b, ob, strict_cohort):
 
     # Infect population 
     for inf in infected:
+
+        # Determine if one of the interactions will be out of cohort
+        if np.random.rand(1) < ob:
+            outside_int = 1
+            inside_int = b-1
+        else:
+            outside_int = 0
+            inside_int = b
         
         # Strict Cohort Policy -- Find members of static cohort
         if strict_cohort == True:
@@ -119,7 +127,7 @@ def infect_pop(pop, b, ob, strict_cohort):
             
             # Identify the b cohort interactions
             # If fewer than b cohort members, just do all members
-            coh_interactions = min(b, len(cohort_mems))
+            coh_interactions = min(inside_int, len(cohort_mems))
             receivers = random.sample(cohort_mems, coh_interactions)
 
             for rec in receivers:
@@ -135,15 +143,15 @@ def infect_pop(pop, b, ob, strict_cohort):
 
             # Identify the b nbr interactions
             # If fewer than b neighbors, just do all neighbors
-            nei_interactions = min(b, len(nbrs))
+            nei_interactions = min(inside_int, len(nbrs))
             receivers = random.sample(nbrs, nei_interactions)
 
             # Infect neighbors
             for rec in receivers:
                 pop[rec].infect()
 
-        # Random check for outside of cohort interaction
-        if np.random.rand(1) < ob:
+        # Interact outside of cohort if check passed
+        if outside_int:
             # Infect a random other agent
             out_i = np.random.choice(nrow)
             out_j = np.random.choice(ncol)
@@ -195,7 +203,7 @@ def get_indices(pop, state):
     indices = [i for i, agent in enumerate(pop_flat) if agent.cur_state() == state]
     return indices
 
-def abm_phase(nrow, ncol, infected, t, bs=np.arange(1, 11, dtype=np.int64), ks=np.linspace(0.01, .5, 10), save_path=None):
+def abm_phase(nrow, ncol, infected, t, bs=np.arange(1, 11, dtype=np.int64), ks=np.linspace(0.01, .5, 10), save_path=None, obs=None, obs_phase=False):
     """
     plot phase diagram
     :param N: starting population
@@ -210,24 +218,37 @@ def abm_phase(nrow, ncol, infected, t, bs=np.arange(1, 11, dtype=np.int64), ks=n
     cts = np.zeros((len(bs), len(ks)))
     N = nrow * ncol
 
-    # Not using ob for the phase plot
-    ob = 0
-
     for i, b in tqdm(enumerate(bs)):
         # ensure b is an int, not a float
-        for j, k in enumerate(ks):
-            pop = new_pop(infected, nrow, ncol)
-            pop, S, I, R = abm_pop_sim(pop, b, ob, k, t)
-            cts[i, j] = np.true_divide(I[-1], N)
+
+        # loop through obs or ks depending on which we want to plot
+        if obs_phase == True:
+            for j, ob in enumerate(obs):
+                pop = new_pop(infected, nrow, ncol)
+                pop, S, I, R = abm_pop_sim(pop, b, ob, .01, t)
+                cts[i, j] = np.true_divide(I[-1], N)
+        else:
+            for j, k in enumerate(ks):
+                pop = new_pop(infected, nrow, ncol)
+                pop, S, I, R = abm_pop_sim(pop, b, 0, k, t)
+                cts[i, j] = np.true_divide(I[-1], N)
 
 
-    # Create a phase plot
+    # Create a phase plot with variation depending on if using k or ob as variable
     fig, ax = plt.subplots()
-    axcontour = ax.contour(ks, bs, cts)
+    if obs_phase == True:
+        axcontour = ax.contour(obs, bs, cts)
+    else:
+        axcontour = ax.contour(ks, bs, cts)
     fig.colorbar(axcontour)
     ax.set_title('phase diagram [I, t={}]'.format(t))
-    ax.set_xlabel('k')
-    ax.set_ylabel('b')
+    if obs_phase == True:
+        ax.set_xlabel('Chance of a Non-local Interaction')
+        ax.set_ylabel('Number of Interactions')
+    else:
+        ax.set_xlabel('k')
+        ax.set_ylabel('b')
+    
 
     # Save plot
     if save_path is not None:
@@ -282,8 +303,7 @@ def time_plot(pop, b, ob, k, t, save_path):
 
 def make_cohorts(pop, size=9):
     """
-    Splits the population grid into square blocks of given size and
-    assigns a cohort to each block.
+    Splits the population into cohorts of a given size
     """
     nrow, ncol = pop.shape
 
