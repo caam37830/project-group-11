fe0df61f1d574a8c2a811e69d43319fa18eee9c7
James Keane <yahmskeano@gmail.com>
2020-12-08 00:21:17 -0600

    Updated scripts and adding images

--- scripts/abm_2D.py
+++ scripts/abm_2D.py
@@ -6,10 +6,44 @@ import numpy as np
 from tqdm import tqdm
 
 
-pop = new_pop(0, 20, 20)
-pop[0,0].pos = (0.5,0.5)
-pop[0,0].infect()
-pop = pop.flatten()
 
 q = np.sqrt(3 / 10000 / np.pi)
-pop, S, I, R = abm_2D_sim(pop, .3, q=q, k=0.01, t=15)
+p = .3
+num_sims = 3
+t = 15
+center = np.zeros(t)
+corners = np.zeros(t)
+randoms = np.zeros(t)
+
+for i in tqdm(range(num_sims)):
+    # Centered infected start
+    pop = new_pop(0,50,50)
+    for i in range(4):
+        pop[i, 0].infect()
+        pop[i, 0].pos = (0.5,0.5)
+    pop, S, I, R = abm_2D_sim(pop, .3, q=q, k=0.1, t=15)
+    center = np.add(center, np.true_divide(S, 50*50))
+
+    # Corner infected start
+    pop = new_pop(0,50,50)
+    for i in range(4):
+        pop[i, 0].infect()
+        pop[i, 0].pos = (0,0)
+    pop, S, I, R = abm_2D_sim(pop, .3, q=q, k=0.1, t=15)
+    corners = np.add(corners, np.true_divide(S, 50*50))
+
+    pop = new_pop(4, 50, 50)
+    pop, S, I, R = abm_2D_sim(pop, .3, q=q, k=0.1, t=15)
+    randoms = np.add(randoms, np.true_divide(S, 50*50))
+
+
+t = np.linspace(1,t,t)
+plt.plot(t, corners/num_sims, label='Center', color='gold')
+plt.plot(t, center/num_sims, label='Corner', color='purple')
+plt.plot(t, randoms/num_sims, label='Random', color='black')
+plt.legend('Infection Start')
+plt.title("Infection Progress in Spatial Model, q={}, p={}".format(q, p))
+plt.ylabel("ratio")
+plt.xlabel("day")
+
+plt.savefig('../doc/checkpoint/figures/spatialComp.png')
--- scripts/abm_cohort_sim.py
+++ scripts/abm_cohort_sim.py
@@ -9,12 +9,12 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 # Create 3 new time plots with outside interaction chance.
-pop = new_pop(1, 100, 100)
-time_plot(pop, 3, 0.01, .01, 50,'../doc/checkpoint/figures/abm3.png')
-pop = new_pop(1, 100, 100)
-time_plot(pop, 3, 0.01, .1, 50,'../doc/checkpoint/figures/abm3.png')
-pop = new_pop(1, 100, 100)
-time_plot(pop, 1, 0.01, .01, 50,'../doc/checkpoint/figures/abm3.png')
+pop = new_pop(5, 100, 100)
+time_plot(pop, 3, 0.0001, .1, 365, m=0,u=0,infectivity=0,risk=0, save_path='../doc/final/figures/neigh1.png')
+pop = new_pop(5, 100, 100)
+time_plot(pop, 3, 0.01, .1, 365, m=0,u=0,infectivity=0,risk=0, save_path='../doc/final/figures/neigh2.png')
+pop = new_pop(5, 100, 100)
+time_plot(pop, 3, 0.3, .1, 365, m=0,u=0,infectivity=0,risk=0, save_path='../doc/final/figures/neigh3.png')
 
 # Create 3 new phase plots to check ob with b
 bs=np.arange(1, 11, dtype=np.int64)
@@ -27,18 +27,18 @@ abm_phase(100, 100, 1, t=50, bs=bs, obs=obs, obs_phase=True, save_path='../doc/c
 
 # Make a heatmap
 I_list = np.empty(shape=1)
-ncol = 90
-nrow = 90
-num_sims = 50
-t = 20
+ncol = 100
+nrow = 100
+num_sims = 100
+t = 15
 
 
 # Simulate a number of epidemics with b=3, ob=.01, k=.01 
 for i in tqdm(range(num_sims)):
-    pop = new_pop(1, nrow, ncol)
+    pop = new_pop(4, nrow, ncol)
     pop, S, I, R = abm_pop_sim(pop, 3, .01, .01, t)
     pop_f = pop.flatten()
-    
+
     # Record the agents that remain susceptible at day t
     I_list = np.append(I_list, get_indices(pop_f, 'S'))
 
@@ -59,40 +59,5 @@ im = ax.imshow(infected_rates, cmap='gist_heat')
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.05)
 plt.colorbar(im, cax=cax)
-plt.savefig('../doc/checkpoint/figures/Var1heatmap.png')
-plt.show()
-
-# Make a heatmap
-I_list = np.empty(shape=1)
-ncol = 90
-nrow = 90
-num_sims = 100
-t = 20
-
-# Simulate a number of epidemics with b=3, ob=.01, k=.01 
-for i in tqdm(range(num_sims)):
-    pop = new_pop(1, nrow, ncol)
-    pop, S, I, R = abm_pop_sim(pop, 3, .05, .01, t, strict_cohort=True, cohort_size=9)
-
-    # Record the agents that remain susceptible at day t
-    I_list = np.append(I_list, get_indices(pop, 'S'))
-
-# Calculate the average rate of infection per agent
-infected_rates = np.empty(shape=(ncol*nrow))
-for i in range(nrow*ncol):
-    suscept_instances = np.array(np.where(I_list == i))
-    susceptible_rate = suscept_instances.shape[1] / num_sims
-    infected_rates[i] = 1 - susceptible_rate
-
-# Plot an infection heat map
-infected_rates = np.reshape(infected_rates, (nrow, ncol))
-ax = plt.subplot(111)
-plt.title('Strict Cohort Infection Rate Heatmap(b=3,ob=.01,k=.01)')
-im = ax.imshow(infected_rates, cmap='gist_heat')
-
-# Add a colorbar
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="5%", pad=0.05)
-plt.colorbar(im, cax=cax)
-plt.savefig('../doc/checkpoint/figures/Var1heatmap.png')
+plt.savefig('../doc/final/figures/Var1heatmap.png')
 plt.show()
