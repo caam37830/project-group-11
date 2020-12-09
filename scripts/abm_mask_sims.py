a251c217ef2b6b9b1d446bfe7cdfa1a9edc21426
Luke Chen <lukechen@uchicago.edu>
2020-12-08 23:47:32 -0600

    final version of plots

--- scripts/abm_mask_sims.py
+++ scripts/abm_mask_sims.py
@@ -17,7 +17,7 @@ time_plot(masked_pop, 3, 1/(nrow*ncol), 0.1, 183, 0.25, 0.05, 0.2, 0.5, dir_path
 pop = new_pop(200, nrow, ncol)
 time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0.25, 0.05, 0.2, 0.5, dir_path+'/doc/final/figures/no_mask2.png')
 
-pop = new_pop(15, nrow, ncol)
+pop = new_pop(500, nrow, ncol)
 time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0.1, np.round(1/365, 3), 1, 0.05, dir_path+'/doc/final/figures/vaccine.png')
 
 abm_mask_phase(np.linspace(0,1,25), np.linspace(0,1,25), 0.2, 0.7, save_path = dir_path+'/doc/final/figures/mask_phase1.png',
