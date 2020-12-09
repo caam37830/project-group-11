4e21322242b37f0ac6fb5f4b9976c9324509324b
Luke Chen <lukechen@uchicago.edu>
2020-12-08 23:19:39 -0600

    altered parameters in visuals

--- scripts/abm_mask_sims.py
+++ scripts/abm_mask_sims.py
@@ -5,20 +5,20 @@ from sir.abm import *
 
 nrow, ncol = 100, 100
 pop = new_pop(15, nrow, ncol)
-time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0, 0, 0.2, 0.7, dir_path+'/doc/final/figures/no_mask1.png')
+time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0, 0, 0.2, 0.5, dir_path+'/doc/final/figures/no_mask1.png')
 
 pop = new_pop(15, nrow, ncol)
-time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0.25, 0.05, 0.2, 0.7, dir_path+'/doc/final/figures/mask1.png')
+time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0.6, 0.05, 0.2, 0.5, dir_path+'/doc/final/figures/mask1.png')
 
 pop = new_pop(200, nrow, ncol)
 masked_pop = update_masks(pop, 0.2, 0.7, 0.67, 0)
-time_plot(masked_pop, 3, 1/(nrow*ncol), 0.1, 183, 0.25, 0.05, 0.2, 0.7, dir_path+'/doc/final/figures/mask2.png')
+time_plot(masked_pop, 3, 1/(nrow*ncol), 0.1, 183, 0.25, 0.05, 0.2, 0.5, dir_path+'/doc/final/figures/mask2.png')
 
 pop = new_pop(200, nrow, ncol)
-time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0.25, 0.05, 0.2, 0.7, dir_path+'/doc/final/figures/no_mask2.png')
+time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0.25, 0.05, 0.2, 0.5, dir_path+'/doc/final/figures/no_mask2.png')
 
 pop = new_pop(15, nrow, ncol)
-time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0.01, np.round(1/365, 3), 1, 0.05, dir_path+'/doc/final/figures/vaccine.png')
+time_plot(pop, 3, 1/(nrow*ncol), 0.1, 183, 0.1, np.round(1/365, 3), 1, 0.05, dir_path+'/doc/final/figures/vaccine.png')
 
 abm_mask_phase(np.linspace(0,1,25), np.linspace(0,1,25), 0.2, 0.7, save_path = dir_path+'/doc/final/figures/mask_phase1.png',
 nrow=50, ncol=50)
